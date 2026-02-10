279be4fe2f5b8321f4dfd051de475808de597e94
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2026-02-10 18:23:56 +0800

    feat: packages and codes for lab2

---
+++ Lab2_kvsrv/client.go
@@ -0,0 +1,82 @@
+package kvsrv
+
+import (
+	"Lab2_kvsrv/labrpc"
+	"crypto/rand"
+	"math/big"
+	"sync/atomic"
+)
+
+type Clerk struct {
+	server *labrpc.ClientEnd
+	// You will have to modify this struct.
+	ClientId int64
+	OpId     atomic.Int64
+}
+
+func nrand() int64 {
+	max := big.NewInt(int64(1) << 62)
+	bigx, _ := rand.Int(rand.Reader, max)
+	x := bigx.Int64()
+	return x
+}
+
+func MakeClerk(server *labrpc.ClientEnd) *Clerk {
+	ck := new(Clerk)
+	ck.server = server
+	// You'll have to add code here.
+	ck.ClientId = nrand()
+	ck.OpId.Store(0)
+	return ck
+}
+
+// fetch the current value for a key.
+// returns "" if the key does not exist.
+// keeps trying forever in the face of all other errors.
+//
+// you can send an RPC with code like this:
+// ok := ck.server.Call("KVServer.Get", &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) Get(key string) string {
+	opid := ck.OpId.Add(1)
+	args := GetArgs{Key: key, OpId: opid, ClientId: ck.ClientId}
+	var reply GetReply
+	ok := false
+
+	for !ok {
+		ok = ck.server.Call("KVServer.Get", &args, &reply)
+	}
+	return reply.Value
+}
+
+// shared by Put and Append.
+//
+// you can send an RPC with code like this:
+// ok := ck.server.Call("KVServer."+op, &args, &reply)
+//
+// the types of args and reply (including whether they are pointers)
+// must match the declared types of the RPC handler function's
+// arguments. and reply must be passed as a pointer.
+func (ck *Clerk) PutAppend(key string, value string, op string) string {
+	opid := ck.OpId.Add(1)
+	args := PutAppendArgs{Key: key, Value: value, OpId: opid, ClientId: ck.ClientId}
+	var reply PutAppendReply
+	ok := false
+
+	for !ok {
+		ok = ck.server.Call("KVServer."+op, &args, &reply)
+	}
+	return reply.Value
+}
+
+func (ck *Clerk) Put(key string, value string) {
+	ck.PutAppend(key, value, "Put")
+}
+
+// Append value to key's value and return that value
+func (ck *Clerk) Append(key string, value string) string {
+	return ck.PutAppend(key, value, "Append")
+}
---
+++ Lab2_kvsrv/go.mod
@@ -0,0 +1,3 @@
+module Lab2_kvsrv
+
+go 1.25.7
