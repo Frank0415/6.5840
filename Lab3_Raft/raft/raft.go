605a003170ba405908ebaa82dd75a3e504808a32
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2026-02-18 00:42:35 +0800

    feat: 3D finish

--- Lab3_Raft/raft/raft.go
+++ Lab3_Raft/raft/raft.go
@@ -116,8 +116,6 @@ func (rf *Raft) readPersist(data []byte) {
 	}
 }
 
-
-
 // the service using Raft (e.g. a k/v server) wants to start
 // agreement on the next command to be appended to Raft's log. if this
 // server isn't the leader, returns false. otherwise start the
